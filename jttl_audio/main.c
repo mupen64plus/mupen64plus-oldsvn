83acc750c0e58196f88719ab6d96eea5eb29d8ca
Richard Goedeken <Richard@fascinationsoftware.com>
2008-04-15 20:13:28 +0000

    Renamed some poorly-named variables.  Removed unneeded globals.  Added speed control functionality.  Speed control should now be complete but is not tested

--- jttl_audio/main.c
+++ jttl_audio/main.c
@@ -146,11 +146,11 @@ static Uint8 *buffer = NULL;
 /* Position in buffer array where next audio chunk should be placed */
 static unsigned int buffer_pos = 0;
 /* Audio frequency, this is usually obtained from the game, but for compatibility we set default value */
-static int frequency = DEFAULT_FREQUENCY;
+static int GameFreq = DEFAULT_FREQUENCY;
 /* This is for syncronization, it's ticks saved just before AiLenChanged() returns. */
 static Uint32 last_ticks = 0;
-/* This is amount of ticks that are needed for previous audio chunk to be played */
-static Uint32 expected_ticks = 0;
+/* SpeedFactor is used to increase/decrease game playback speed */
+static Uint32 speed_factor = 100;
 // AI_LEN_REG at previous round */
 static DWORD prev_len_reg = 0;
 // If this is true then left and right channels are swapped */
@@ -166,7 +166,7 @@ static Uint32 HighBufferLoadLevel = HIGH_BUFFER_LOAD_LEVEL;
 // Resample or not
 static Uint8 Resample = 1;
 
-static int realFreq;
+static int OutputFreq;
 static char configdir[PATH_MAX] = {0};
 
 /* ----------- FUNCTIONS ------------- */
@@ -177,7 +177,7 @@ void InitializeSDL();
 
 EXPORT void CALL AiDacrateChanged( int SystemType )
 {
-    int f = frequency;
+    int f = GameFreq;
     switch (SystemType)
     {
         case SYSTEM_NTSC:
@@ -256,18 +256,27 @@ EXPORT void CALL AiLenChanged( void )
 
     if (wait_time != -1)
     {
+        /* Adjust the game frequency by the playback speed factor for the purposes of timing */
+        int InputFreq = GameFreq * speed_factor / 100;
+
         /* If for some reason game is runnin extremely fast and there is risk buffer is going to
            overflow, we slow down the game a bit to keep sound smooth. The overspeed is caused
            by inaccuracy in machines clock. */
-        if(buffer_pos > HighBufferLoadLevel)
+        if (buffer_pos > HighBufferLoadLevel)
         {
-            wait_time += (float)(buffer_pos - HIGH_BUFFER_LOAD_LEVEL) / (float)(frequency / 250);
+            int overflow = (buffer_pos - HIGH_BUFFER_LOAD_LEVEL) / 4; /* in samples */
+            wait_time += overflow * 1000 / InputFreq;                 /* in milliseconds */
         }
-        expected_ticks = ((float)(prev_len_reg) / (float)(frequency / 250));
+        
+        /* calculate how many milliseconds should have elapsed since the last audio chunk was added */
+        int prev_samples = prev_len_reg / 4;
+        int expected_ticks = prev_samples * 1000 / InputFreq;  /* in milliseconds */
 
-        if (last_ticks + expected_ticks > SDL_GetTicks())
+        /* now determine if we are ahead of schedule, and if so, wait */
+        int cur_ticks = SDL_GetTicks();
+        if (last_ticks + expected_ticks > cur_ticks)
         {
-            wait_time += (last_ticks + expected_ticks) - SDL_GetTicks();
+            wait_time += (last_ticks + expected_ticks) - cur_ticks;
 #ifdef DEBUG
             printf("[JttL's SDL Audio plugin] Debug: wait_time: %i, Buffer: %i/%i\n", wait_time, buffer_pos, PrimaryBufferSize);
 #endif
@@ -277,7 +286,6 @@ EXPORT void CALL AiLenChanged( void )
 
     last_ticks = SDL_GetTicks();
     prev_len_reg = LenReg;
-
 }
 
 EXPORT DWORD CALL AiReadLength( void )
@@ -390,7 +398,7 @@ EXPORT void CALL DllTest ( HWND hParent )
     obtained = malloc(sizeof(SDL_AudioSpec));
     
     // 22050Hz - FM Radio quality
-    desired->freq=frequency;
+    desired->freq=GameFreq;
 
     // Print out message for frequency
     printf("[JttL's SDL Audio plugin] Requesting frequency: %iHz.\n", desired->freq);
@@ -538,11 +546,9 @@ unsigned int _dest_len = 0;
 int error;
 SRC_STATE *src_state;
 SRC_DATA src_data;
-int newsamplerate;
-int oldsamplerate;
 #endif
 
-static void resample(Uint8 *src, int src_len, Uint8 *dest, int dest_len)
+static void resample(Uint8 *src, int src_len, int oldsamplerate, Uint8 *dest, int dest_len, int newsamplerate)
 {
 #ifdef USE_SRC
     if(Resample == 2)
@@ -615,16 +621,14 @@ static void resample(Uint8 *src, int src_len, Uint8 *dest, int dest_len)
 
 void my_audio_callback(void *userdata, Uint8 *stream, int len)
 {
-    if(buffer_pos > (len * frequency) / realFreq)
-    {
-#ifdef USE_SRC
-        newsamplerate = realFreq;
-        oldsamplerate = frequency;
-#endif
-        int rlen = ((len/4 * frequency) / realFreq)*4;
-        resample(buffer, rlen, stream, len);
-        memmove(buffer, &buffer[ rlen ], buffer_pos  - rlen);
+    int newsamplerate = OutputFreq * 100 / speed_factor;
+    int oldsamplerate = GameFreq;
 
+    if (buffer_pos > (len * oldsamplerate) / newsamplerate)
+    {
+        int rlen = ((len/4 * oldsamplerate) / newsamplerate) * 4;
+        resample(buffer, rlen, oldsamplerate, stream, len, newsamplerate);
+        memmove(buffer, &buffer[ rlen ], buffer_pos  - rlen);
         buffer_pos = buffer_pos - rlen ;
     }
     else
@@ -633,7 +637,7 @@ void my_audio_callback(void *userdata, Uint8 *stream, int len)
         underrun_count++;
         fprintf(stderr, "[JttL's SDL Audio plugin] Debug: Audio buffer underrun (%i).\n",underrun_count);
 #endif
-        memset(stream + ((buffer_pos/4 * realFreq) / frequency)*4, 0, len - ((buffer_pos/4 * realFreq) / frequency)*4);
+        memset(stream + ((buffer_pos/4 * newsamplerate) / oldsamplerate)*4, 0, len - ((buffer_pos/4 * newsamplerate) / oldsamplerate)*4);
         buffer_pos = 0;
     }
 }
@@ -641,7 +645,7 @@ EXPORT void CALL RomOpen()
 {
     /* This function is for compatibility with Mupen64. */
     ReadConfig();
-    InitializeAudio( frequency );
+    InitializeAudio( GameFreq );
 }
 void InitializeSDL()
 {
@@ -680,7 +684,7 @@ void InitializeAudio(int freq)
         InitializeSDL();
     }
 
-    frequency = freq; // This is important for the sync
+    GameFreq = freq; // This is important for the sync
     if(hardware_spec != NULL) free(hardware_spec);
     SDL_PauseAudio(1);
     SDL_CloseAudio();
@@ -700,11 +704,11 @@ void InitializeAudio(int freq)
     // 22050Hz - FM Radio quality
     //desired->freq=freq;
     
-    if(freq < 11025) realFreq = 11025;
-    else if(freq < 22050) realFreq = 22050;
-    else realFreq = 44100;
+    if(freq < 11025) OutputFreq = 11025;
+    else if(freq < 22050) OutputFreq = 22050;
+    else OutputFreq = 44100;
     
-    desired->freq = realFreq;
+    desired->freq = OutputFreq;
     
 #ifdef DEBUG
     printf("[JttL's SDL Audio plugin] Debug: Requesting frequency: %iHz.\n", desired->freq);
@@ -792,6 +796,12 @@ EXPORT void CALL ProcessAlist( void )
 {
 }
 
+EXPORT void CALL SetSpeedFactor(int percentage)
+{
+    if (percentage >= 10 && percentage <= 300)
+        speed_factor = percentage;
+}
+
 void ReadConfig()
 {
     FILE * config_file;
@@ -826,7 +836,7 @@ void ReadConfig()
 #ifdef DEBUG
             printf("[JttL's SDL Audio plugin] Debug: Parameter \"%s\", value: \"%i\"\n",&param,atoi(&value[1]));
 #endif
-            if(strcasecmp(param, "DEFAULT_FREQUENCY") == 0) frequency = atoi(value);
+            if(strcasecmp(param, "DEFAULT_FREQUENCY") == 0) GameFreq = atoi(value);
             if(strcasecmp(param, "SWAP_CHANNELS") == 0) SwapChannels = atoi(value);
             if(strcasecmp(param,"PRIMARY_BUFFER_SIZE") == 0) PrimaryBufferSize = atoi(value);
             if(strcasecmp(param,"SECONDARY_BUFFER_SIZE") == 0) SecondaryBufferSize = atoi(value);
