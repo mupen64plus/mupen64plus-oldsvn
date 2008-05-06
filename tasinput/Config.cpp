b957164f7825437f8a6b946a417ffa7091ee6c0f
Scott Gorman <okaygo@localhost>
2008-05-06 05:04:31 +0000

    fixed a silly error

--- tasinput/Config.cpp
+++ tasinput/Config.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	Modified for TAS by Nitsuja
 */
 
+#include "../main/winlnxdefs.h"
 #include <windows.h>
 #include <commctrl.h>
 #define DIRECTINPUT_VERSION 0x0700
--- tasinput/Config.h
+++ tasinput/Config.h
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef _CONFIG_H_INCLUDED__
 #define _CONFIG_H_INCLUDED__
 
-#include "../main/winlnxdefs.h"
 
 #define IDT_TIMER1 1
 #define IDT_TIMER2 2
