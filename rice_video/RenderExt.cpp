269a94cecc9f32c60b48c3ded44f4768a09721a7
Richard Goedeken <Richard@fascinationsoftware.com>
2008-05-15 19:33:23 +0000

    Cleaned up some weird code

--- rice_video/RenderExt.cpp
+++ rice_video/RenderExt.cpp
@@ -405,8 +405,9 @@ void CRender::DrawSpriteR(uObjTxSprite &sprite, bool initCombiner, uint32 tile, 
     }
 
     //RECT src = {left,top,width, height};
-    float depth;
-    (gRDP.otherMode.depth_source==1) ? depth = gRDP.fPrimitiveDepth : depth = 0;
+    float depth = 0.0;
+    if (gRDP.otherMode.depth_source==1) 
+        depth = gRDP.fPrimitiveDepth;
 
     float x0 = sprite.sprite.objX/4.0f;
     float y0 = sprite.sprite.objY/4.0f;
