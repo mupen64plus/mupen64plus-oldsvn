07a9a1008d3360f06ab424d3e00a8e399d223b6a
Richard Goedeken <Richard@fascinationsoftware.com>
2008-08-07 04:13:56 +0000

    fixed blight input licensing issues

--- blight_input/Controller_1.1.h
+++ blight_input/Controller_1.1.h
@@ -1,10 +1,26 @@
-/**********************************************************************************
-Common Controller plugin spec, version #1.1 maintained by 
-zilmar (zilmar@emulation64.com)
+/**
+ * Mupen64Plus - Controller_1.1.h
+ * Copyright (C) 2002 Zilmar
+ *
+ * Mupen64Plus homepage: http://code.google.com/p/mupen64plus/
+ * 
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * Licence along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301, USA
+ *
+**/
 
-All questions or suggestions should go through the emutalk plugin forum.
-http://www.emutalk.net/cgi-bin/ikonboard/ikonboard.cgi?s=3bd272222f66ffff;act=SF;f=20
-**********************************************************************************/
 #ifndef _CONTR_H_INCLUDED__
 #define _CONTR_H_INCLUDED__
 
--- blight_input/configdialog_sdl.c
+++ blight_input/configdialog_sdl.c
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          configdialog_sdl.c
+                             -------------------
+ Copyright (C) 2002 Gregor Anich (blight)
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifdef GUI_SDL
 
 #include "configdialog_sdl.h"
