50a925fb76f6ccf41148486cf73d8585043194c6
Scott Knauert <Tillin9@localhost>
2008-08-14 20:06:04 +0000

    License fixes.

--- install.sh
+++ install.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-#
-# mupen64plus install script
-#
+#/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# *   Mupen64plus - install.sh                                              *
+# *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+# *   Copyright (C) 2007-2008 Richard42                                     *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+# Mupen64plus install script.
 
 if [ -x /usr/bin/ginstall ]; then
 	INSTALL=/usr/bin/ginstall
@@ -82,9 +100,6 @@ $INSTALL -d -v "${SHAREDIR}/icons/16x16" || exit $?
 $INSTALL -m 0644 icons/16x16/* "${SHAREDIR}/icons/16x16" || exit $?
 $INSTALL -d -v "${SHAREDIR}/lang" || exit $?
 $INSTALL -m 0644 lang/* "${SHAREDIR}/lang" || exit $?
-if [ "${GUI}" == "KDE4" ]; then
-    $INSTALL -m 0644 mupen64plusui.rc "${SHAREDIR}" || exit $?
-fi
 
 $INSTALL -D -m 0755 mupen64plus "${BINDIR}/mupen64plus" || exit $?
 $INSTALL -d -v "${MANDIR}" || exit $?
--- mupen64_input/Controller_1.1.h
+++ mupen64_input/Controller_1.1.h
@@ -1,10 +1,24 @@
-/**********************************************************************************
-Common Controller plugin spec, version #1.1 maintained by 
-zilmar (zilmar@emulation64.com)
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   Mupen64plus - Controller_1.1.h                                        *
+ *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+ *   Copyright (C) 2002 Zilmar                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-All questions or suggestions should go through the emutalk plugin forum.
-http://www.emutalk.net/cgi-bin/ikonboard/ikonboard.cgi?s=3bd272222f66ffff;act=SF;f=20
-**********************************************************************************/
 #ifndef _CONTR_H_INCLUDED__
 #define _CONTR_H_INCLUDED__
 
