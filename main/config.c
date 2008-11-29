45bda08231dbae9e080d2c92176e1454ec8369ef
Richard Goedeken <Richard@fascinationsoftware.com>
2008-11-29 14:27:22 +0000

    bugfix: crash in rev 1154 when ~/.mupen64plus not present

--- main/config.c
+++ main/config.c
@@ -112,7 +112,8 @@ const char * config_get_string( const char *key, const char *def )
     SConfigValue *val = config_findValue( key );
     if (!val)
     {
-        config_put_string(key, def);
+        if (def != NULL)
+            config_put_string(key, def);
         return def;
     }
 
